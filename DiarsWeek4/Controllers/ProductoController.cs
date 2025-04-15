94dc8efa4e53be1b23d0ad6d2cdde1d1bb1c668a
franklinnot <franklinnot@outlook.com>
2025-04-14 21:00:00 -0500

    controlador para listar y modificar productos

---
+++ DiarsWeek4/Controllers/ProductoController.cs
@@ -0,0 +1,68 @@
+﻿using DiarsWeek4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiarsWeek4.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Listar productos
+        public async Task<IActionResult> Index()
+        {
+            var productos = await _context.Productos.ToListAsync();
+            return View(productos);
+        }
+
+        // GET: Producto/Edit/<id>
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+                return NotFound();
+
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/<id>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Sku,Nombre,Stock,Precio,Estado")] Producto producto)
+        {
+            if (id != producto.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return _context.Productos.Any(e => e.Id == id);
+        }
+    }
+}
