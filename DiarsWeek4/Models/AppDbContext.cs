6860ee9a16fb225c25b4a2f2757ef9f4ac3d2bdb
frank <frank@BREAK>
2025-04-14 20:39:21 -0500

    Add project files.

---
+++ DiarsWeek4/Models/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiarsWeek4.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=DBProductos;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Estado)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("estado");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(128)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Precio).HasColumnName("precio");
+            entity.Property(e => e.Sku)
+                .HasMaxLength(128)
+                .HasColumnName("sku");
+            entity.Property(e => e.Stock).HasColumnName("stock");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DiarsWeek4/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiarsWeek4.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Sku { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public int Stock { get; set; }
+
+    public double Precio { get; set; }
+
+    public string Estado { get; set; } = null!;
+}
